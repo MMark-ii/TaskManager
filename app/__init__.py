5f16a81517435dd5f3c451965631e4ac69dd7f98
Wasa <fenixcyc@student.21_school.ru>
2025-03-23 13:32:27 +0300

    first_commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from .auth.models import User  # Добавьте этот импорт
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 @login_manager.user_loader
 def load_user(user_id):
+    from .auth.models import User  # Переместите импорт сюда
     return User.query.get(int(user_id))
 
 def create_app():
