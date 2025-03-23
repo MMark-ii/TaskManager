57473cb04d625d32104c279bf2f8dcce261eab00
Wasa <fenixcyc@student.21_school.ru>
2025-03-23 13:22:57 +0300

    first_commit

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,15 +1,37 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
+from .forms import LoginForm, RegistrationForm
 from .models import User
+from app import db
 
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint('auth', __name__, template_folder='templates')
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    # Добавьте реализацию логина
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('tasks.task_list'))
+        else:
+            flash('Invalid username or password')
+    return render_template('auth/login.html', form=form)
 
 @auth_bp.route('/logout')
+@login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return redirect(url_for('auth.login'))
+
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
