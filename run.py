11329bffca9db1a75a54b1680dccb2061d251636
Wasa <fenixcyc@student.21_school.ru>
2025-03-23 14:19:41 +0300

    first_commit

--- run.py
+++ run.py
@@ -3,12 +3,10 @@ from app import create_app, db
 
 app = create_app()
 
-# Проверка существования базы данных
-db_path = os.path.join(os.path.dirname(__file__), 'site.db')
-if not os.path.exists(db_path):
-    with app.app_context():
-        db.create_all()
-        print("База данных создана.")
+# Явное создание базы данных
+with app.app_context():
+    db.create_all()
+    print("База данных создана.")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
