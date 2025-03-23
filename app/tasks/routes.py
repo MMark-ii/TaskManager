b9b88fd93f749357f0e0ff666a936aad34fdb2c6
Wasa <fenixcyc@student.21_school.ru>
2025-03-23 14:45:59 +0300

    first_commit

--- app/tasks/routes.py
+++ app/tasks/routes.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_required
-from app.tasks.models import Task  # ← Исправлен импорт
+from .models import Task
+from app import db
 
-# Инициализация Blueprint должна быть в начале
 tasks_bp = Blueprint('tasks', __name__, template_folder='templates')
 
 @tasks_bp.route('/tasks')
@@ -10,3 +10,15 @@ tasks_bp = Blueprint('tasks', __name__, template_folder='templates')
 def task_list():
     tasks = Task.query.all()
     return render_template('tasks/list.html', tasks=tasks)
+
+@tasks_bp.route('/tasks/add', methods=['GET', 'POST'])
+@login_required
+def add_task():
+    if request.method == 'POST':
+        title = request.form['title']
+        new_task = Task(title=title)
+        db.session.add(new_task)
+        db.session.commit()
+        flash('Task added successfully')
+        return redirect(url_for('tasks.task_list'))
+    return render_template('tasks/add.html')
